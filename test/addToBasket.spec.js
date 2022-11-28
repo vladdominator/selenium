a5b5083efc104c5291a0d0c6be127c9f6a695ca2
vladdominator <vlad.zhilinskiy2003@inbox.ru>
2022-11-28 08:44:33 +0300

    fix: add chromedriver locally

--- test/addToBasket.spec.js
+++ test/addToBasket.spec.js
@@ -3,13 +3,13 @@ import {Browser, Builder, Capabilities} from "selenium-webdriver";
 import {ProductPage} from "../pages/ProductPage.js";
 import {BasketPage} from "../pages/BasketPage.js";
 import {DEFAULT_BASKET_PAGE_URL, DEFAULT_PRODUCT_PAGE_URL, DEFAULT_PRODUCT_SIZE} from "../constants/constants.js";
-import chrome from 'selenium-webdriver/chrome.js';
-
-const service = new chrome.ServiceBuilder('node_modules/chromedriver/lib/chromedriver/chromedriver');
 
 describe('Add to Basket.', () => {
     beforeEach(async function () {
-        this.driver = await new Builder().usingServer('http://localhost:4444/wd/hub').forBrowser(Browser.CHROME).setChromeService(service).build();
+        const capabilities = {
+            ...Capabilities.chrome(),
+        };
+        this.driver = await new Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(capabilities).forBrowser(Browser.CHROME).build();
         await this.driver.manage().window().maximize();
     });
 
